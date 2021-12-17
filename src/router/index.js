5d88148a9cd1f6d6dfba8bae8d928176e3b1c4a6
sun yu <1838125172@qq.com>
2021-12-17 10:07:20 +0800

    day01sunyu

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,11 @@
+//引入vue和路由对象
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import routes from './routes'
+//安装路由插件
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    //向外暴露路由
+export default new VueRouter({
+    mode: 'history',
+    routes
 })
-
-export default router
---
+++ src/router/routes.js
@@ -0,0 +1,18 @@
+export  default [
+    {
+      path:'/',
+      component: () => import( '@/pages/Home'),
+    },
+    {
+      path:'/search',
+      component: () => import( '@/pages/Search'),
+    },
+    {
+      path:'/login',
+      component: () => import( '@/pages/Login'),
+    },
+    {
+      path:'/register',
+      component: () => import( '@/pages/Register'),
+    },
+  ]
